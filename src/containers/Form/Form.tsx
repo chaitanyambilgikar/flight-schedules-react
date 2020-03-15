f8f5f1c92afc4cefb0a957f64efc97e0803df293
Chaitanya Bilgikar <chaitanya@MacBook-Pro.local>
2020-03-15 09:19:53 -0400

    Add validation to form

--- src/containers/Form/Form.tsx
+++ src/containers/Form/Form.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Form, FormikProps, Formik } from 'formik';
+import { Form, FormikProps, Formik, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
 import SearchBar from '../SearchBar';
 
 type FormType = {
@@ -8,19 +9,32 @@ type FormType = {
 };
 interface IFormValues {
   departureAirport: string;
+  arrivalAirport: string;
 }
+
+const ScheduleSearchSchema = Yup.object().shape({
+  departureAirport: Yup.string()
+    .required('required'),
+  arrivalAirport: Yup.string()
+    .required('required')
+});
+
 const UnstyledForm: React.FunctionComponent<FormType> = ({
   className,
   ...props
 }) => {
-  const initialValues: IFormValues = { departureAirport: 'YYZ' };
+  const initialValues: IFormValues = { departureAirport: '', arrivalAirport: '' };
   return (
     <div className={className}>
       <h1>My Form</h1>
       <Formik
         initialValues={initialValues}
-        onSubmit={(values, actions) => {
+        validationSchema={ScheduleSearchSchema}
+        validateOnChange={false}
+        validateOnBlur={false}
+        onSubmit={(values: IFormValues, formikHelpers: FormikHelpers<IFormValues>) => {
           console.log('Values: ', values);
+          formikHelpers.setSubmitting(false);
         }}
       >
         {(props: FormikProps<IFormValues>) => (
@@ -30,7 +44,23 @@ const UnstyledForm: React.FunctionComponent<FormType> = ({
               type='search'
               placeholder='Departure Airport, eg: YYZ'
               maxLength={3}
+              size={30}
             />
+            {
+              props.errors.departureAirport && props.touched.departureAirport ?
+              <div>{props.errors.departureAirport}</div> : null
+            }
+            <SearchBar
+              name='arrivalAirport'
+              type='search'
+              placeholder='Arrival Airport, egL LHR'
+              maxLength={3}
+              size={30}
+            />
+            {
+              props.errors.arrivalAirport && props.touched.arrivalAirport ?
+              <div>{props.errors.arrivalAirport}</div> : null
+            }
             <button type='submit'>Submit</button>
           </Form>
         )}
